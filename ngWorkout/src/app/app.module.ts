2ac5bf742783e6f94966396e879e1a4b013ae7bf
TaylorY1 <YockeyT139@gmail.com>
2022-05-21 09:19:26 -0600

    Initial Commit

---
+++ ngWorkout/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { WorkoutsService } from './services/workouts.service';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    WorkoutsService,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
